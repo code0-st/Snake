87c84455a0c11f3d626d18590b36c2004324028e
Kirill Chumarenko <chumarenko_kir@mail.ru>
2020-08-05 15:38:12 +0300

    v.0.5 Added snake tail and its moving

--- js/index.js
+++ js/index.js
@@ -1,66 +1,91 @@
 'use strict'
 
 const canvas = document.querySelector('.ground'),
-    ctx = canvas.getContext("2d");
+ctx = canvas.getContext("2d");
 
-const playerVelocity = 10;
-const playerSize = 40;
-const FPS = 60;
-let directionX = 0;
-let directionY = -1;
-let currentX = (canvas.clientWidth - playerSize) / 2;
-let currentY = (canvas.clientHeight - playerSize) / 2;
+const grid = 30;
+const FPS = 20;
 
-const DrawBegin = () => {
-    ctx.fillStyle = "rgb(200,0,0)";
+const player = {
+    dx: 0,
+    dy: -grid,
+    currentX: (canvas.clientWidth - grid) / 2,
+    currentY: (canvas.clientHeight - grid) / 2,
 
-    ctx.fillRect(currentX, currentY, playerSize, playerSize);
-}
-
-const Draw = () => {
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect(currentX += playerVelocity*directionX, currentY += playerVelocity*directionY, playerSize, playerSize);    
-}
-
-const Update = () => {
-    Draw();
-    teleport();
-    console.log(`current X: ${currentX}
-    current Y: ${currentY}`);
-}
+    tail: [],
+    tailLength: 80
+};
 
 const Start = () => {
     DrawBegin();
     setInterval(Update, 1000 / FPS);
-}
+};
+
+const Update = () => {
+    Draw();
+    teleport();
+};
+
+const DrawBegin = () => {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+};
+
+const Draw = () => {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight); // Очистка поля
+    move();
+};
 
 const teleport = () => {
-    currentX = currentX < 0 ? canvas.clientWidth - playerSize : currentX > canvas.clientWidth - playerSize ? 0 : currentX;
-    currentY = currentY < 0 ? canvas.clientHeight - playerSize : currentY > canvas.clientHeight - playerSize ? 0 : currentY;
+    player.currentX = player.currentX < 0 ? canvas.clientWidth : player.currentX > canvas.clientWidth ? -grid : player.currentX;
+    player.currentY = player.currentY < 0 ? canvas.clientHeight : player.currentY > canvas.clientHeight ? -grid : player.currentY;
+};
+
+const move = () => {
+      
+    player.currentX += player.dx;
+    player.currentY += player.dy;
+
+    player.tail.unshift({
+        x: player.currentX,
+        y: player.currentY
+    });
+
+    if (player.tail.length > player.tailLength) {
+        player.tail.pop();
+    }
+
+    player.tail.forEach((cell, i) => {
+        ctx.fillStyle = `rgb(${24+i*3}, ${36+i*10}, ${151+i*5})`;
+        ctx.fillRect(cell.x, cell.y, grid, grid);
+        console.log(`PlayerX: ${player.currentX}, PlayerY: ${player.currentY}
+            CellX: ${cell.x}, CellY: ${cell.y}
+            DX: ${player.currentX - cell.x}, DY: ${player.currentY - cell.y}`);
+    });
+};
+
+
+const changeDirection = key => {
+    switch(key) {
+        case 'KeyW': player.dx = 0; player.dy = -grid; 
+            //ctx.fillRect(currentX, currentY += velocity*directionY, 50, 50);
+            break;
+        case 'KeyS': player.dx = 0; player.dy = grid; 
+            //ctx.fillRect(currentX, currentY += velocity*directionY, 50, 50);
+            break;
+        case 'KeyA': player.dx = -grid; player.dy = 0;
+            //ctx.fillRect(currentX += velocity*directionX, currentY, 50, 50);
+            break;
+        case 'KeyD': player.dx = grid; player.dy = 0;
+            //ctx.fillRect(currentX += velocity*directionX, currentY, 50, 50);
+            break;
+    }  
 }
 
 document.addEventListener('load', Start());
 
 document.addEventListener('keydown', e => {
     console.log(e.code);
-    Move(e.code);
+    changeDirection(e.code);
 })
 
 
-function Move( key) {
-    switch(key) {
-        case 'KeyW': directionY = -1; directionX = 0;
-            //ctx.fillRect(currentX, currentY += velocity*directionY, 50, 50);
-            break;
-        case 'KeyS': directionY = 1; directionX = 0;
-            //ctx.fillRect(currentX, currentY += velocity*directionY, 50, 50);
-            break;
-        case 'KeyA': directionX = -1; directionY = 0;
-            //ctx.fillRect(currentX += velocity*directionX, currentY, 50, 50);
-            break;
-        case 'KeyD': directionX = 1; directionY = 0;
-            //ctx.fillRect(currentX += velocity*directionX, currentY, 50, 50);
-            break;
-    }  
-}
